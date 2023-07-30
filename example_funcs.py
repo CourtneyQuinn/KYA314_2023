3f0fe368631445f0f536a9b07423c0351a40bb1a
CourtneyQuinn <40574281+CourtneyQuinn@users.noreply.github.com>
2023-07-31 09:46:38 +1000

    Update example_funcs.py
    Added Henon Map

--- example_funcs.py
+++ example_funcs.py
@@ -39,3 +39,34 @@ def LogisticMap(x,lam):
     xout = lam*x[0,:]*(1-x[0,:]);
     
     return xout
+
+def HenonMap(x,p):
+    '''2D map exhibiting complex behaviour
+    Input
+    ----------
+    x : array (2, N)
+        Points to iterate
+        If N = 1, x is one point
+    p : array (2,)
+        parameter values
+    Returns
+    -------
+    xout : array, shape (2, N)
+           Array containing 1-step interate of map
+           2nd dimension denotes iterates for different starting points
+    '''
+
+    if x.ndim == 1:
+        x = np.expand_dims(x, axis=1)
+    
+    n = x.shape
+    xout = np.empty(n)
+    xout[:] = np.nan 
+    
+    alpha = p[0]
+    beta = p[1]
+    
+    xout[0,:] = 1 - alpha*x[0,:]**2+x[1,:];
+    xout[1,:] = beta*x[0,:]
+    
+    return xout
