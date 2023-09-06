f71fff7c808894359e889617babcaa1093d30ff9
CourtneyQuinn <40574281+CourtneyQuinn@users.noreply.github.com>
2023-09-06 15:42:26 +1000

    varargin interpreter for MyTrackCurve
    Use as:
    
    default={'stepsize',0.01,'nmax',100,'tol',1e-5,'maxit',10};
    options=MySetOptions(default,varargin);
    s = options.stepsize;
    nmax = options.nmax;
    tol = options.tol;
    maxit = options.maxit;

---
+++ MATLAB/MySetOptions.m
@@ -0,0 +1,34 @@
+function [options,passed_on]=MySetOptions(defaults,userargs,pass_on)
+%% parses optional arguments of a function and assigns them as fields of
+%% structure options
+% unknown arguments are passed on into cell array passed_on if pass_on is
+% present and non-empty or false, otherwise and error message is generated
+passed_on={};
+% wrap cell arguments to avoid generating multiple structs
+if isstruct(defaults)
+    options=defaults;
+elseif iscell(defaults)
+    for i=1:length(defaults)
+        if iscell(defaults{i})
+            defaults{i}={defaults{i}};
+        end
+    end
+    options=struct(defaults{:});
+else
+    error('defaults not recognized\n');
+end
+if nargin<3 || isempty(pass_on)
+    pass_on=false;
+end
+for i=1:2:length(userargs)
+    if isfield(options,userargs{i})
+        options.(userargs{i})=userargs{i+1};
+    else
+        if ~pass_on
+            error('option ''%s'' not recognized\n',userargs{i});
+        else
+            passed_on={passed_on{:},userargs{i},userargs{i+1}};
+        end
+    end
+end
+end
